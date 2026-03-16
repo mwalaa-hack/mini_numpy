f21ee2c1be394fb6bb218e54264d30888ca5c5ee
Esraa Mohamed <Cds.EsraaMohamed250771@alexu.edu.eg>
2026-03-16 23:57:18 +0200

    Update ndarray.py

--- core/ndarray.py
+++ core/ndarray.py
@@ -1,8 +1,8 @@
-from exceptions.errors import NonNumericDataError
+from exceptions.errors import DimensionMismatchError, InvalidOperationError
 
 class Array:
     def __init__(self, data):
-        self._check_numeric(data)   
+        self._check_numeric(data)
         self.data = data
         self.shape = self._get_shape(data)
         self.ndim = len(self.shape)
@@ -12,7 +12,7 @@ class Array:
             for x in item:
                 self._check_numeric(x)
         elif not isinstance(item, (int, float)):
-            raise NonNumericDataError()
+            raise NonNumericDataError(item)
 
     def _get_shape(self, data):
         shape = []
@@ -35,3 +35,39 @@ class Array:
 
     def __repr__(self):
         return f"Array(shape={self.shape})\n{self.data}"
+
+ 
+    def _apply(self, other, op):
+        # Other is also an Array
+        if isinstance(other, Array):
+            if self.shape != other.shape:
+                raise DimensionMismatchError()
+            def recurse(a, b):
+                if isinstance(a, list):
+                    return [recurse(x, y) for x, y in zip(a, b)]
+                return op(a, b)           
+            return Array(recurse(self.data, other.data))
+
+        # Other is a number
+        elif isinstance(other, (int, float)):
+            def recurse(a):
+                if isinstance(a, list):
+                    return [recurse(x) for x in a]
+                return op(a, other)
+            return Array(recurse(self.data))
+
+        else:
+            raise InvalidOperationError()
+
+    # Magic methods all use _apply 
+    def __add__(self, other):
+        return self._apply(other, lambda a, b: a + b)
+
+    def __sub__(self, other):
+        return self._apply(other, lambda a, b: a - b)
+
+    def __mul__(self, other):
+        return self._apply(other, lambda a, b: a * b)
+
+    def __pow__(self, other):
+        return self._apply(other, lambda a, b: a ** b)
