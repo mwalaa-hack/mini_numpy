941a40323f87921a8de6621a809ef81536a178b3
Esraa Mohamed <Cds.EsraaMohamed250771@alexu.edu.eg>
2026-03-17 00:11:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,34 +4,34 @@ from exceptions.errors import DimensionMismatchError, NonNumericDataError, Inval
 from core.ndarray import Array
 
 def main():
-    # a1 = array([1,2,3])
-    # # print(a1)
+    a1 = array([1,2,3])
+    print(a1)
 
-    # a2 = array([[1,2],[3,4]])
-    # # print(a2)
+    a2 = array([[1,2],[3,4]])
+    print(a2)
 
-    # z = zeros((2,2,2))
-    # # print(z)
+    z = zeros((2,2,2))
+    print(z)
 
-    # o = ones((3,2))
-    # print(o)
+    o = ones((3,2))
+    print(o)
 
-    # e = eye(3)
-    # # print(e)
+    e = eye(3)
+    print(e)
 
-    # a = array([[1,2,3],[4,5,6]])
+    a = array([[1,2,3],[4,5,6]])
 
-    # print("Array:")
-    # print(a)
+    print("Array:")
+    print(a)
 
-    # print("\nMean:")
-    # print(mean(a))
+    print("\nMean:")
+    print(mean(a))
 
-    # print("\nVariance:")
-    # print(var(a))
+    print("\nVariance:")
+    print(var(a))
 
-    # print("\nStandard Deviation:")
-    # print(std(a))
+    print("\nStandard Deviation:")
+    print(std(a))
 
     try:
         raise DimensionMismatchError()
